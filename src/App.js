e855c8d59d0b4fada21baa80819180d72ae2d466
Luke Berry <lukeberrypi@gmail.com>
2024-08-07 00:18:36 +0200

    Revert "fix: casing"
    This reverts commit e80f55c458cceed156d90c9fec70f8156b82a727.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import PopularMoviesPage from "./modules/popularMovies/PopularMovies.page.jsx";
 import SearchMoviesPage from "./modules/searchMovies/SearchMovies.page.jsx";
 import WatchLaterPage from "./modules/watchLaterMovies/WatchLaterMovies.page.jsx";
 import Header from "./shared/components/header/Header.jsx";
-import "./app.scss";
+import "./App.scss";
 import useDebounce from "./shared/hooks/useDebounce.js";
 
 const App = () => {
