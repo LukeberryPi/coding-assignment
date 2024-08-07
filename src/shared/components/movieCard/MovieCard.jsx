a38968c590df468e2f62533a00852e73bf6449e5
Luke Berry <lukeberrypi@gmail.com>
2024-08-08 01:47:50 +0200

    feat: add remove logic

--- src/shared/components/movieCard/MovieCard.jsx
+++ src/shared/components/movieCard/MovieCard.jsx
@@ -1,4 +1,4 @@
-import { Clock, Play, Star } from "lucide-react";
+import { Play, Clock, Star } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
@@ -13,18 +13,38 @@ import "./MovieCard.scss";
 
 const MovieCard = ({ movie, onPosterClick }) => {
   const dispatch = useDispatch();
-  const { id, poster_path, title } = movie;
+  const { favoriteMovies } = useSelector((state) => state.favoriteMovies);
+  const { watchLaterMovies } = useSelector((state) => state.watchLaterMovies);
+  const { poster_path, title } = movie;
+
+  const isFavoriteMovie = favoriteMovies?.some(
+    (favoriteMovie) => favoriteMovie.id === movie.id,
+  );
+
+  const isWatchLaterMovie = watchLaterMovies?.some(
+    (watchLaterMovie) => watchLaterMovie.id === movie.id,
+  );
+
+  const hasPoster = !!poster_path;
 
   const getImageURL = (poster_path) => {
     return `https://image.tmdb.org/t/p/w500/${poster_path}`;
   };
 
   const handleStarClick = () => {
+    if (isFavoriteMovie) {
+      dispatch(removeFavoriteMovie(movie));
+      return;
+    }
     dispatch(addFavoriteMovie(movie));
   };
 
   const handleClockClick = () => {
-    dispatch(removeFavoriteMovie(movie));
+    if (isWatchLaterMovie) {
+      dispatch(removeWatchLaterMovie(movie));
+      return;
+    }
+    dispatch(addWatchLaterMovie(movie));
   };
 
   return (
@@ -33,14 +53,18 @@ const MovieCard = ({ movie, onPosterClick }) => {
         onClick={() => onPosterClick(movie)}
         className="movie-card__poster-button"
       >
-        <img
-          className="movie-card__image"
-          src={getImageURL(poster_path)}
-          alt={`${title} poster`}
-        />
+        {hasPoster ? (
+          <img
+            className="movie-card__image"
+            src={getImageURL(poster_path)}
+            alt={`${title} poster`}
+          />
+        ) : (
+          <div className="movie-card__poster-placeholder">No poster found</div>
+        )}
         <div className="movie-card__overlay">
           <Play />
-          View Trailer
+          <span>View Trailer</span>
         </div>
       </button>
       <div className="movie-card__details-container">
@@ -49,14 +73,17 @@ const MovieCard = ({ movie, onPosterClick }) => {
         </div>
         <div className="movie-card__actions">
           <button onClick={handleStarClick}>
-            <Star />
-            add
-            {/* <span>{isFavoriteMovie ? "Remove" : "Add"}</span> */}
+            <Star
+              fill={isFavoriteMovie ? "orange" : "none"}
+              style={{ color: `${isFavoriteMovie ? "orange" : "white"}` }}
+            />
+            <span>{isFavoriteMovie ? "Remove" : "Add"}</span>
           </button>
           <button onClick={handleClockClick}>
-            <Star />
-            remove
-            {/* <span>{isWatchLaterMovie ? "Remove" : "Add"}</span> */}
+            <Clock
+              style={{ color: `${isWatchLaterMovie ? "blueviolet" : "white"}` }}
+            />
+            <span>{isWatchLaterMovie ? "Remove" : "Add"}</span>
           </button>
         </div>
       </div>
